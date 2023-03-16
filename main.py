4a157a8dce7e3e1350643f12f856b2c33b40ac45
Sergei Zelentsov <serge.anselmo@gmail.com>
2023-03-16 17:44:31 +0400

    modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ ui.icon('thumb_up')
 #add a button that plays Rick Astley's Never Gonna Give You Up
 ui.button('Never Gonna Give You Up',on_click=lambda: ui.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
 ui.label('Your operating system is ' + osmodel)
-app = ui.run()
+app = ui.run(port=8000)
