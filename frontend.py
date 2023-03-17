f8b133d0d62b6a9a3adee065320ef7687ae694fe
Sergei Zelentsov <serge.anselmo@gmail.com>
2023-03-17 18:39:19 +0400

    last coomit

--- frontend.py
+++ frontend.py
@@ -10,7 +10,7 @@ osmodel = platform.system()
 def init(app: FastAPI) -> None:
     @ui.page('/show')
     def show():
-        ui.label('Hello, RickRoller!')
+        ui.label('Hello, RickRoller!', style='h1')
         ui.icon('thumb_up')
         #add GIF with rickroll
         ui.image('https://media.tenor.com/x8v1oNUOmg4AAAAM/rickroll-roll.gif', )
