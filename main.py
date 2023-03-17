d6c769a3fcab0c61041ac5eb5b8b60f1ce96cc88
Sergei Zelentsov <serge.anselmo@gmail.com>
2023-03-17 18:30:11 +0400

    last commit

--- main.py
+++ main.py
@@ -15,6 +15,5 @@ frontend.init(app)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
-#run with uvicorn main:app --reload
-#I want to run this via Gunicorn, but I don't know how to do it.
+#I want to run this via Gunicorn in Azure App Service, do this:
 #gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app
