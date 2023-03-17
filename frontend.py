b35624ae1846c58f29dacea681da8f2072e1a4ec
Sergei Zelentsov <serge.anselmo@gmail.com>
2023-03-17 18:21:25 +0400

    modified:   frontend.py

--- frontend.py
+++ frontend.py
@@ -10,13 +10,12 @@ osmodel = platform.system()
 def init(app: FastAPI) -> None:
     @ui.page('/show')
     def show():
-        ui.label('Hello, FastAPI!')
+        ui.label('Hello, RickRoller!')
         ui.icon('thumb_up')
         #add GIF with rickroll
-        ui.image('https://media.tenor.com/x8v1oNUOmg4AAAAM/rickroll-roll.gif')
+        ui.image('https://media.tenor.com/x8v1oNUOmg4AAAAM/rickroll-roll.gif', )
         ui.button('Never Gonna Give You Up',on_click=lambda: ui.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
         ui.label('Your operating system is ' + osmodel)
-
     ui.run_with(app)
 
 
